c05bce49c54d8b27efce0c6fdb29d0a8db45c879
Maciej Zolnierczyk <maciej.zolnierczyk@studenci.collegiumwitelona.pl>
2024-12-20 13:08:34 +0100

    Kebab strucutre continued

--- app/Models/Kebab.php
+++ app/Models/Kebab.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Kebab extends Model
 {
     use HasFactory;
+
+    protected $table = 'kebab';
+
+    protected $fillable = [
+        'name',
+        'logo',
+        'address',
+        'latitude',
+        'longitude',
+        'year_opened',
+        'year_closed',
+        'opening_hours',
+        'meat_types',
+        'sauces',
+        'status',
+        'is_craft',
+        'is_in_stall',
+        'is_chain_member',
+        'ordering_options',
+    ];
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Http\Controllers\KebabController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+
+
+
+
+
+
+
+Route::group(["middleware" => ["auth:sanctum"]], function() {
+    Route::get('/showAllKebab/{id}', [KebabController::class, 'showAllKebab']);
+    Route::get('/showKebab/{id}', [KebabController::class, 'showKebab']);
+    Route::post('/addKebab', [KebabController::class, 'addKebab']);
+    Route::put('/updateKebab/{id}', [KebabController::class, 'updateKebab']);
+    Route::delete('/delKebab/{id}', [KebabController::class, 'delKebab']);
+});
+//Route::get('/user', function (Request $request) {return $request->user();})->middleware('auth:sanctum');
