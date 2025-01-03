549f3dded646279fe8fa5b8b9f533de3674ca356
Maciej Zolnierczyk <maciej.zolnierczyk@studenci.collegiumwitelona.pl>
2025-01-03 12:45:15 +0100

    defined suggestion migration

--- database/migrations/2025_01_03_114255_create_suggestions_table.php
+++ database/migrations/2025_01_03_114255_create_suggestions_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('suggestions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->text('content');
+            $table->boolean('is_reviewed')->default(false);
             $table->timestamps();
         });
     }
