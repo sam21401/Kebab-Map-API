bb370cec7b4a815f7edb0fb9770444f355ede194
Maciej Zolnierczyk <maciej.zolnierczyk@studenci.collegiumwitelona.pl>
2025-01-03 12:49:57 +0100

    defined suggestion model

--- app/Models/Suggestion.php
+++ app/Models/Suggestion.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Suggestion extends Model
 {
     use HasFactory;
+
+    protected $table = 'suggestions';
+
+    protected $fillable = [
+        'user_id',
+        'content',
+        'is_reviewed',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
